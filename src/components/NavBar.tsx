1faf11b7682af53199d4a5d222c49002461f2ad2
saddsoul <wojciechsitko1@gmail.com>
2024-03-05 21:48:03 +0100

    internationalization - styles, to be finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,11 @@
 import Image from "next/image";
 import { Link } from "../navigation";
 import LogoUnique from '../../public/svg/LogoUnique.svg'
+import pl from '../../public/svg/pl.svg'
+import de from '../../public/svg/de.svg'
+import gb from '../../public/svg/gb.svg'
 import LangSwitch from "./LanguageSwitcher";
+
 
 export default function Navbar() {
     return (
@@ -12,7 +16,7 @@ export default function Navbar() {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-80">
                         <li>
                             <Link href="/">Strona główna</Link>
                         </li>
@@ -25,6 +29,27 @@ export default function Navbar() {
                         <li>
                             <a href="https://drive.google.com/drive/folders/1X_KE1MfTXSugQNiO3Hy71jtR0mPBp7MU">Katalog</a>
                         </li>
+                        <div className="h-px w-full bg-gray-600"></div>
+                        <div className="flex flex-row my-2">
+                            <Image
+                                priority
+                                src={pl}
+                                alt="pl"
+                                className="h-4 w-auto mx-5"
+                            />
+                            <Image
+                                priority
+                                src={gb}
+                                alt="gb"
+                                className="h-4 w-auto"
+                            />
+                            <Image
+                                priority
+                                src={de}
+                                alt="de"
+                                className="h-4 w-auto mx-5 "
+                            />
+                        </div>
                     </ul>
                 </div>
                 <Image
